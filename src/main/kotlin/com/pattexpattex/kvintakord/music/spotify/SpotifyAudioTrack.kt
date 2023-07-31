21916e4afb103aea7d9c218722cb5e9f5677d9e3
PattexPattex <bojan.brdar.turk@gmail.com>
2023-07-31 14:55:06 +0200

    Override SpotifyAudioTrack#makeShallowClone

--- src/main/kotlin/com/pattexpattex/kvintakord/music/spotify/SpotifyAudioTrack.kt
+++ src/main/kotlin/com/pattexpattex/kvintakord/music/spotify/SpotifyAudioTrack.kt
@@ -4,9 +4,14 @@ import com.adamratzman.spotify.models.Track
 import com.pattexpattex.kvintakord.music.player.TrackMetadata
 import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager
 import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioTrack
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack
 
-class SpotifyAudioTrack(baseAudioTrack: YoutubeAudioTrack?, backingTrack: Track) : YoutubeAudioTrack(baseAudioTrack?.info, baseAudioTrack?.sourceManager as? YoutubeAudioSourceManager) {
+class SpotifyAudioTrack(private val baseAudioTrack: YoutubeAudioTrack?, private val backingTrack: Track) : YoutubeAudioTrack(baseAudioTrack?.info, baseAudioTrack?.sourceManager as? YoutubeAudioSourceManager) {
     init {
         TrackMetadata.buildForSpotify(this, backingTrack)
+    }
+
+    override fun makeShallowClone(): AudioTrack {
+        return SpotifyAudioTrack(baseAudioTrack, backingTrack)
     }
 }
