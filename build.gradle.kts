bc0c7f8f44ad48b3bea382e2bfdddce78aa9e8a6
PattexPattex <bojan.brdar.turk@gmail.com>
2023-09-09 19:58:38 +0200

    Update libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     application
     `maven-publish`
-    kotlin("jvm") version "1.8.10"
-    id("org.openjfx.javafxplugin") version "0.0.13"
+    kotlin("jvm") version "1.9.10"
+    id("org.openjfx.javafxplugin") version "0.1.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
@@ -25,19 +25,19 @@ javafx {
 
 dependencies {
     implementation("com.github.edvin:tornadofx2:master-SNAPSHOT")
-    implementation("com.github.walkyst:lavaplayer-fork:1.4.0")
-    implementation("com.adamratzman:spotify-api-kotlin-core:4.0.0")
+    implementation("dev.arbjerg:lavaplayer:2.0.1")
+    implementation("com.adamratzman:spotify-api-kotlin-core:4.0.2")
     implementation("com.vdurmont:emoji-java:5.1.1")
     implementation("com.dustinredmond.fxtrayicon:FXTrayIcon:4.0.1")
-    implementation("org.slf4j:jul-to-slf4j:2.0.7")
-    runtimeOnly("ch.qos.logback:logback-classic:1.4.6")
-    runtimeOnly("org.fusesource.jansi:jansi:1.17")
+    implementation("org.slf4j:jul-to-slf4j:2.0.9")
+    runtimeOnly("ch.qos.logback:logback-classic:1.4.11")
+    runtimeOnly("org.fusesource.jansi:jansi:2.4.0")
 
     //Fix vulnerabilities
-    runtimeOnly("com.fasterxml.jackson.core:jackson-databind:2.14.2")
-    runtimeOnly("commons-codec:commons-codec:1.15")
+    runtimeOnly("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    runtimeOnly("commons-codec:commons-codec:1.16.0")
     runtimeOnly("org.apache.httpcomponents:httpclient:4.5.14")
-    runtimeOnly("org.json:json:20230227")
+    runtimeOnly("org.json:json:20230618")
 
     runtimeOnly("org.openjfx:javafx-graphics:${javafx.version}:win")
     runtimeOnly("org.openjfx:javafx-graphics:${javafx.version}:linux")
